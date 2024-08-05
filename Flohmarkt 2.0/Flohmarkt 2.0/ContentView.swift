e81af4f9f042708e9fdc88396edb30d9db39d2a9
MarcelSchwabe <marcel.schwabe1982@gmail.com>
2024-08-05 14:39:02 +0200

    Start Projekt

---
+++ Flohmarkt 2.0/Flohmarkt 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flohmarkt 2.0
+//
+//  Created by Marcel Schwabe on 05.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flohmarkt 2.0/Flohmarkt 2.0/Flohmarkt_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Flohmarkt_2_0App.swift
+//  Flohmarkt 2.0
+//
+//  Created by Marcel Schwabe on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Flohmarkt_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
